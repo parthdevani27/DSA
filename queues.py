0a791567f3d3288fccfafd636f3dc7fa0e0c21a0
Parth <parthdevani2121@gmail.com>
2024-03-19 22:44:43 +0530

    queue

---
+++ queues.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+class Queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+        else:
+            self.last.next = new_node
+        self.last = new_node    
+        self.length += 1
+        return True
+    
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = temp.next
+            temp.next = None
+        self.length -= 1
+        return temp
+        
+q = Queue(7)
+q.enqueue(6)
+q.enqueue(5)
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.enqueue(2)
+q.print_queue()
